0b62ee77213579b9117387476513c67b4c4ede4c
Wilhelm Sæhl Olsen <wilhelol@stud.ntnu.no>
2024-09-29 19:59:53 +0200

    updated azurerm version

--- module06/staticwebsite/providers.tf
+++ module06/staticwebsite/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.73.0"
+      version = "4.2.0"
       skip_provider_registration = true
     }
   }
@@ -16,5 +16,6 @@ terraform {
 }
 
 provider "azurerm" {
+  subscription_id = "30ee9279-e76e-409d-8973-00c9792f6bcb"
   features {}
 }
