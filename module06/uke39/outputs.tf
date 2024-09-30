52e742680fb8b9268fcebab5c7c1035be4f7bb3e
Wilhelm Sæhl Olsen <wilhelol@stud.ntnu.no>
2024-09-30 17:03:09 +0200

    del

--- module06/uke39/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "storage_account_name" {
-  value = azurerm_storage_account.sa.name
-}
-
-output "storage_account_primary_endpoint" {
-  value = azurerm_storage_account.sa.primary_endpoints
-}
