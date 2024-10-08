96204b4f996845ae3876aa18c467999ddada4da4
Wilhelm Sæhl Olsen <wilhelol@stud.ntnu.no>
2024-10-08 23:06:46 +0200

    uke40

---
+++ module07/demo-infra/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.2.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tfstate-wso"
+    storage_account_name = "sabetfsdn6p8"
+    container_name       = "tfstate"
+    key                  = "web.terraform.tfstate"
+    # Credentials will be pulled from environment variables
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "30ee9279-e76e-409d-8973-00c9792f6bcb"
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
