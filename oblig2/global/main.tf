9b512183c019a9d5e7a0090868f7ab59b17f0cb2
Wilhelm Sæhl Olsen <wilhelol@stud.ntnu.no>
2024-11-05 16:57:23 +0100

    Set up file structure

---
+++ oblig2/global/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.2.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "wso-rg-backend"
+    storage_account_name = "sabetfsaini2"
+    container_name       = "tfstate"
+    key                  = "oblig2.terraform.tfstate"
+    # Credentials will be pulled from environment variables
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "30ee9279-e76e-409d-8973-00c9792f6bcb"
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
